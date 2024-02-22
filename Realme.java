8a01e25d39015818860e4bcd79a9f52c35f42972
Maulana ifratsim <156280371+Kwaci-Kwacu@users.noreply.github.com>
2024-02-22 09:20:39 +0700

    Realme.java

--- Realme.java
+++ Realme.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.main;
 
-/**
- *
- * @author victu
- */
-public class Realme {
+
     
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+       
+        Phone Realme1 = new Realme();
+
+      
+        PhoneUser ifrat = new PhoneUser(Realme1);
+
+     
+            ifrat.turnOnThePhone();
+        
+        
+        Scanner input = new Scanner(System.in);
+        String aksi; 
+
+        while (true) {
+            System.out.println("=== APLIKASI INTERFACE ===");
+            System.out.println("[1] Power On HP");
+            System.out.println("[2] Power Off HP");
+            System.out.println("[3] Tambah Volume");
+            System.out.println("[4] Kuranggi Volume");
+            System.out.println("[0] Keluar");
+            System.out.println("--------------------------");
+            System.out.print("Pilih aksi> ");
+            aksi = input.nextLine();
+            
+            if(aksi.equalsIgnoreCase("1")){
+                    Yahya.turnOnThePhone();
+            } else if (aksi.equalsIgnoreCase("2")){
+                    Yahya.turnOffThePhone();
+            } else if (aksi.equalsIgnoreCase("3")){
+                    Yahya.makePhoneLouder();
+            } else if (aksi.equalsIgnoreCase("4")){
+                    Yahya.makePhoneSilent();
+            } else if (aksi.equalsIgnoreCase("0")){
+                System.exit(0);
+            } else {
+                System.out.println("ERORRR! ");
+            }
+        }
+
+    }
+
 }
+
